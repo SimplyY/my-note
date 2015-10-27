dcbb1b9489361febb98caa1f0e7c2c1e2cd90f9c
SimplyY <SimplyYu@163.com>
2015-10-27 19:17:00 +0800

    add getQureyParams js util

---
+++ web/js/util.js
@@ -0,0 +1,18 @@
+// url = http://127.0.0.1:8080/JnPlant/scene?scene_id=1&user_id=2
+// return Object {scene_id: "1", user_id: "2"}
+function getQureyParams(url) {
+    var searchParams = {};
+
+    var qurey = url.split('?');
+    // scene_id=1&user_id=2
+    qurey = qurey[qurey.length -1];
+
+    var params = qurey.split('&');
+    for (var i = 0; i < params.length; i++) {
+        // scene_id=1
+        var keyValue = params[i].split('=');
+        searchParams[keyValue[0]] = keyValue[1];
+    }
+
+    return searchParams;
+}
